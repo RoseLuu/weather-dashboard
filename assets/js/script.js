506d896c9820562d4c832534e2bb42f9b5e758b5
Mila Decker <milacdecker@gmail.com>
2020-11-17 18:38:00 -0500

    updated API fetches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
 var cityInput = document.querySelector('#city');
 var cityBtn = document.querySelector('#search-btn');
+var cityForecastEl = document.querySelector('.city-forecast');
+var currentWeatherEl = document.querySelector('.current-weather');
+var cityNameEl = document.querySelector('.city-name');
 
 var formHandler = function(event) {
     event.preventDefault();
-    var cityName = cityInput.value.trim();
+    var selectedCity = cityInput.value.trim();
 
-    if (cityName) {
-        getCityWeather(cityName);
+    if (selectedCity) {
+        getCoords(selectedCity);
         cityInput.value = '';
     } else {
         alert('Please enter a city!');
     }
 }
 
-var getCityWeather = function(city) {
-    var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3655ed2750f651629b08c7f61f4fd95c`;
-    var fiveDayApi = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=0231b088137927c3d579de4869b3ea5f`;
+// uses 'current weather api' to pull latitude and longitude
+var getCoords = function(city) {
+    var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=3655ed2750f651629b08c7f61f4fd95c`;
 
     fetch(currentWeatherApi).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data)
-                // displayCity(data, city);
+                var lon = data.coord['lon'];
+                var lat = data.coord['lat'];
+
+                formatCityName(city);
+                getCityForecast(lon, lat);
             });
         } else {
             alert(`Error: ${response.statusText}`)
@@ -30,19 +36,35 @@ var getCityWeather = function(city) {
     .catch(function(error) {
         alert('Unable to load weather.');
     })
+}
 
-    fetch(fiveDayApi).then(function(response) {
+// uses latitude and longitude to pull current weather and five-day forecast
+var getCityForecast = function(lon, lat, city) {
+    var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=0231b088137927c3d579de4869b3ea5f`;
+    fetch(oneCallApi).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data)
-                // display fiveDay(data, city)
+
+                displayForecast(data, city)
             });
         }
     })
-    .catch(function(error) {
-        alert('Unable to load weather.');
-    })
 }
 
+// formats and displays city name
+var formatCityName = function(city) {
+    var cityName = city.toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+
+    cityNameEl.textContent = `${cityName} at ${moment().format("h:mm A on M/D/YYYY")}`;
+}
+
+// displays forecast for city
+// var displayForecast = function(forecast) {
+
+
+// }
 
 cityBtn.addEventListener("click", formHandler)
